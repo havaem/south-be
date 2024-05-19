d1fb039da2a54c43ee1dac9b9668c95ff214989d
havaem <vhnvohoainam@gmail.com>
2024-05-19 10:41:06 +0700

    refactor: 💡 adding new enum locale

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,7 @@
 export * from "./action";
 export * from "./api";
 export * from "./casl";
+export * from "./locale";
 export * from "./permission";
 export * from "./regex";
 export * from "./role";
