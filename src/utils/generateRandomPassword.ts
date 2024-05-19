92cc5e4e7d415481bcfea4d6cf10927ee100415d
havaem <vhnvohoainam@gmail.com>
2024-05-19 10:38:27 +0700

    feat: add func random password

---
+++ src/utils/generateRandomPassword.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param length length of the password
+ * @returns random password
+ */
+export function generateRandomPassword(length: number): string {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+}
